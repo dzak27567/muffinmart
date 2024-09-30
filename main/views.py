3dd62702634fc2cac3c66c23ea4d3a564642f0d2
dzaki <fadhlurohmandzaki@gmail.com>
2024-09-30 22:50:42 +0700

    tes hamburger buat mobile

--- main/views.py
+++ main/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import  reverse
+
 
 # Create your views here.
 @login_required(login_url='/login')
@@ -90,3 +92,27 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, id):
+    # Get mood entry berdasarkan id
+    Product = Product.objects.get(pk = id)
+
+    # Set mood entry sebagai instance dari form
+    form = Product(request.POST or None, instance=mood)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+
+def delete_product(request, id):
+    # Get product berdasarkan id
+    product = Product.objects.get(pk = id)
+    # Hapus mood
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
