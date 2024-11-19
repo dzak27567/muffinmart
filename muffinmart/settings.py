c65c9ae3e60f684b37137883263e9f0efba1c0b4
dzak27567 <fadhlurohmandzaki@gmail.com>
2024-11-19 22:40:43 +0700

    test

--- muffinmart/settings.py
+++ muffinmart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!25fbk#*6pl&x9v7go^-nf$_xac6+c#o_co-f4u*cfr2ncytdm
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fadhlurohman-dzaki-muffinmart.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fadhlurohman-dzaki-muffinmart.pbp.cs.ui.ac.id","10.0.2.2"]
 
 
 
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'muffinmart.urls'
@@ -134,3 +137,9 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://fadhlurohman-dzaki-muffinmart.pbp.cs.ui.ac.id", "https://fadhlurohman-dzaki-muffinmart.pbp.cs.ui.ac.id"]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
